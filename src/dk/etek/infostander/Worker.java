76fe144cef02c68b006e4522dd64476c7f3a5c62
Troels Ugilt Jensen <tujensen@gmail.com>
2014-03-21 14:12:01 +0100

    Added getChannel functions

--- src/dk/etek/infostander/Worker.java
+++ src/dk/etek/infostander/Worker.java
@@ -4,6 +4,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.imageio.ImageIO;
@@ -26,22 +27,64 @@ public class Worker implements Runnable {
 	@Override
 	public void run() {
 		// Initial get of channel.
-		images = getChannel();
+		images = getInitialChannel();
 		
 		int index = 0;
 		
 		while (true) {
+			// Display next image in images array.
 			infostander.setNewImage(images.get(index));
-			
+
+			// Get the next index.
 			index = (index + 1) % images.size();
+
+			// Record time before updating channel.
+			Long beforeChannelGetTime = new Date().getTime();
+			
+			// If next index is first, update channel.
+			if (index == 0) {
+				List<BufferedImage> newlist;
+				try {
+					newlist = getChannel();
+
+					images = newlist;
+				} catch (Exception e) {
+					// If exception, do not update list. Use previous and report error.
+				}
+			}
+
+			// Record time after updating channel.
+			Long afterChannelGetTime = new Date().getTime();
 			
 			// Wait for next event.
 			try {
-				Thread.sleep(60000);
-			} catch (InterruptedException e) {}
+				Thread.sleep(60000 - (afterChannelGetTime - beforeChannelGetTime));
+			} catch (InterruptedException e) {
+				
+			}
 		}
 	}
 	
+	private List<BufferedImage> getInitialChannel() {
+		List<BufferedImage> list = new ArrayList<BufferedImage>();
+		try {
+			List<BufferedImage> newList = getChannel();
+			list = newList;
+		} catch (Exception e) {
+			// Get cached channel.
+			list = getCachedChannel();
+		}
+		return list;
+	}
+	
+	private List<BufferedImage> getCachedChannel() {
+		return new ArrayList<BufferedImage>();
+	}
+	
+	/**
+	 * Get the channel.
+	 * @return
+	 */
 	private List<BufferedImage> getChannel() {
 		// Get channel.
 		
